250d5eef008428d7c4dbe335294f3d6e6db596d9
pikachu0310 <sunnon@i.softbank.jp>
2023-05-02 01:01:41 +0900

    8081 -> 8080

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -43,8 +43,8 @@ func main() {
 
 	go handlePlayerUpdates()
 
-	fmt.Println("Server listening on :8081")
-	err := http.ListenAndServe(":8081", nil)
+	fmt.Println("Server listening on :8080")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
